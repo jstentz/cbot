8b0f7967d4442e481b4fe7a548dfdfbb01fd10d2
Jason Stentz <jstentz@andrew.cmu.edu>
2022-05-11 23:35:35 -0400

    some moving working

--- engine.cpp
+++ engine.cpp
@@ -18,6 +18,7 @@ enum square { A1, B1, C1, D1, E1, F1, G1, H1,
 
 enum rank { RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8 };
 enum file { FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H };
+enum piece { NONE, P, N, B, R, Q, K, p, n, b, r, q, k };
 
 typedef struct Board
 {
@@ -38,6 +39,8 @@ typedef struct Board
     bitboard white_pieces;
     bitboard black_pieces;
     bitboard all_pieces;
+
+    piece sq_board[64];
 } board_t;
 
 typedef struct LUTs {
@@ -55,6 +58,8 @@ typedef struct LUTs {
 typedef struct move_struct {
     square start;
     square target;
+
+    piece captured;
 } move_t;
 
 lut_t *init_LUT () {
@@ -100,6 +105,11 @@ board_t *zero_board() {
     board->white_pieces = 0;
     board->black_pieces = 0;
     board->all_pieces = 0;
+
+    for (size_t i = 0; i < 64; i++) {
+        board->sq_board[i] = NONE;
+    }
+
     return board;
 }
 
@@ -132,10 +142,135 @@ uint16_t last_set_bit(bitboard bits) {
     return count;
 }
 
+bitboard place_piece(bitboard board, square sq, lut_t *luts) {
+    return board | luts->pieces[sq];
+}
+
+bitboard rem_piece(bitboard board, square sq, lut_t *luts) {
+    return board & ~(luts->pieces[sq]);
+}
+
+void make_move(board_t *board, move_t move, lut_t *luts) {
+
+    square start = move.start;
+    square target = move.target;
+
+    piece mv_piece = board->sq_board[start];
+    piece tar_piece = board->sq_board[target];
+
+    switch(mv_piece) {
+        case P :
+            board->white_pawns = rem_piece(board->white_pawns, start, luts);
+            board->white_pawns = place_piece(board->white_pawns, target, luts);
+            break;
+        case N :
+            board->white_knights = rem_piece(board->white_knights, start, luts);
+            board->white_knights = place_piece(board->white_knights, target, luts);
+            break;
+        case B :
+            board->white_bishops = rem_piece(board->white_bishops, start, luts);
+            board->white_bishops = place_piece(board->white_bishops, target, luts);
+            break;
+        case R :
+            board->white_rooks = rem_piece(board->white_rooks, start, luts);
+            board->white_rooks = place_piece(board->white_rooks, target, luts);
+            break;
+        case Q :
+            board->white_queens = rem_piece(board->white_queens, start, luts);
+            board->white_queens = place_piece(board->white_queens, target, luts);
+            break;
+        case K :
+            board->white_kings = rem_piece(board->white_kings, start, luts);
+            board->white_kings = place_piece(board->white_kings, target, luts);
+            break;
+        
+        case p :
+            board->black_pawns = rem_piece(board->black_pawns, start, luts);
+            board->black_pawns = place_piece(board->black_pawns, target, luts);
+            break;
+        case n :
+            board->black_knights = rem_piece(board->black_knights, start, luts);
+            board->black_knights = place_piece(board->black_knights, target, luts);
+            break;
+        case b :
+            board->black_bishops = rem_piece(board->black_bishops, start, luts);
+            board->black_bishops = place_piece(board->black_bishops, target, luts);
+            break;
+        case r :
+            board->black_rooks = rem_piece(board->black_rooks, start, luts);
+            board->black_rooks = place_piece(board->black_rooks, target, luts);
+            break;
+        case q :
+            board->black_queens = rem_piece(board->black_queens, start, luts);
+            board->black_queens = place_piece(board->black_queens, target, luts);
+            break;
+        case k :
+            board->black_kings = rem_piece(board->black_kings, start, luts);
+            board->black_kings = place_piece(board->black_kings, target, luts);
+            break;
+    }
+    
+    switch(tar_piece) {
+        case NONE :
+            break;
+        case P :
+            board->white_pawns = rem_piece(board->white_pawns, target, luts);
+            break;
+        case N :
+            board->white_knights = rem_piece(board->white_knights, target, luts);
+            break;
+        case B :
+            board->white_bishops = rem_piece(board->white_bishops, target, luts);
+            break;
+        case R :
+            board->white_rooks = rem_piece(board->white_rooks, target, luts);
+            break;
+        case Q :
+            board->white_queens = rem_piece(board->white_queens, target, luts);
+            break;
+        case K :
+            board->white_kings = rem_piece(board->white_kings, target, luts);
+            break;
+        
+        case p :
+            board->black_pawns = rem_piece(board->black_pawns, target, luts);
+            break;
+        case n :
+            board->black_knights = rem_piece(board->black_knights, target, luts);
+            break;
+        case b :
+            board->black_bishops = rem_piece(board->black_bishops, target, luts);
+            break;
+        case r :
+            board->black_rooks = rem_piece(board->black_rooks, target, luts);
+            break;
+        case q :
+            board->black_queens = rem_piece(board->black_queens, target, luts);
+            break;
+        case k :
+            board->black_kings = rem_piece(board->black_kings, target, luts);
+            break;
+    }
+
+    board->sq_board[move.start] = NONE;
+    board->sq_board[move.target] = mv_piece;
+    update_boards(board);
+    return;
+}
+
 int main(){
     board_t *board = zero_board();
     lut_t *luts = init_LUT();
-    cout << hex << uppercase << luts->pieces[63] << endl;
+
+    board->white_knights = place_piece(board->white_knights, A1, luts);
+    board->sq_board[A1] = N;
+    move_t move;
+    for (size_t i = 0; i < 63; i++) {
+        move.start = (square)i;
+        move.target = (square)(i+1);
+        make_move(board, move, luts);
+        cout << hex << uppercase << board->white_knights << endl;
+    }
 
     int x;
     cin >> x;
@@ -150,5 +285,20 @@ int main(){
         - Maybe have a move struct and then store an array of moves in a 
           given position. Then you only need to malloc one array.
         - Draw things out on iPad
+        - Have a piece-wise representation in order to figure out what piece is
+          captured.
+        - Have the make move function not even check for legality, that should
+          be the job of the move generation.
+        - Store the moves from generation in a vector (need to import library?)
+        - DON'T create new copies of the board, instead make the move struct
+          hold enough info to be able to undo the move
+        - ^^^ need to be sure to undo the move in recursive calls
+        - make LUT on the stack cuz it doesn't change
+        - ^^^ maybe not tho cuz its large
+        - for dealing with captured pieces, maybe make the board struct hold
+          the most recent captured pieces (NONE if none captured) in order to 
+          undo the move
+
+
 
 */

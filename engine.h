9482138afe5694287a9171a0d716bf0010972375
Jason Stentz <jstentz@andrew.cmu.edu>
2022-05-22 09:43:27 -0400

    graphics stuff and different compiling method

---
+++ engine.h
@@ -0,0 +1,11 @@
+/**
+ * @file engine.h
+ * @author Jason Stentz (jstentz@andrew.cmu.edu)
+ * @brief Provides an interface for the chess engine
+ * @version 0.1
+ * @date 2022-05-22
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+

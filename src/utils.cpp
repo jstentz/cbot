07e7ec1bb0ec1a8be300f85e1ef3be2b949f8419
Jason Stentz <jstentz@andrew.cmu.edu>
2023-07-08 10:41:34 -0400

    board file no longer mad

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,6 +1,48 @@
+#include <iostream>
+
 #include "include/utils.h"
+#include "include/pieces.h"
 
 piece utils::piece_from_fen_char(char c)
 {
-  piece pc{};
+  piece color = islower(c) ? BLACK : WHITE; 
+  piece type;
+  switch (tolower(c))
+  {
+    case 'p':
+      type = PAWN;
+      break;
+    case 'n':
+      type = KNIGHT;
+      break;
+    case 'b':
+      type = BISHOP;
+      break;
+    case 'r':
+      type = ROOK;
+      break;
+    case 'q':
+      type = QUEEN;
+      break;
+    case 'k':
+      type = KING;
+    default: 
+      std::cerr << "Invalid piece name!" << std::endl;
+      exit(1);
+  };
+  return color | type;
+}
+
+Board::Square utils::sq_from_name(std::string name)
+{
+  if (name.size() != 2)
+  {
+    std::cerr << "Invalid square name!" << std::endl;
+    exit(1);
+  }
+
+  int file = constants::FILES.find(name[0]);
+  int rank = constants::RANKS.find(name[1]);
+
+  return (Board::Square)(rank * 8 + file); 
 }

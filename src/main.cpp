edc6e3245feeb2d4bcaa523b9143c7dcddde4a9e
Jason Stentz <jstentz@andrew.cmu.edu>
2023-07-16 13:35:50 -0400

    fixed some bugs

--- src/main.cpp
+++ src/main.cpp
@@ -9,36 +9,58 @@
 
 int main() 
 {
-  /// TODO: boot the engine here 
-  // luts = init_LUT(); // must do this first
-  // zobrist_table = init_zobrist();
-  // // opening_book = create_opening_book(); // uncomment if you need to update opening_book
-  // // generate_num_data(); // uncomment if you need to update opening_book
-  // opening_book = populate_opening_book();
-  // init_tt_table();
-  // init_eval_table();
-  /* begin UCI listening */
+  // std::string test_pos_1 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+  std::string test_pos_2 = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
+  std::string test_pos_3 = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1";
+  std::string test_pos_4 = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1";
+  std::string test_pos_5 = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8";
+  std::string test_pos_6 = "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10";
 
-  Board::Ptr board = std::make_shared<Board>();
+  std::string debug_white_promo = "2n4k/1P6/8/8/8/8/8/7K w - - 0 1";
 
-  MoveGenerator gen{board};
-  for ( ;; )
+  Board::Ptr board = std::make_shared<Board>(debug_white_promo);
+
+  std::cout << board->to_string() << std::endl;
+  
+  for (int i = 2; i < 14; i++)
   {
-    std::cout << board->to_string();
-    std::cout << board->can_white_king_side_castle() << board->can_white_queen_side_castle() << board->can_black_king_side_castle() << board->can_black_queen_side_castle() << std::endl;
-    std::vector<Move> moves;
-    gen.generate_moves(moves);
-    for (int i = 0; i < moves.size(); i++)
-    {
-      std::cout << i << ": " << gen.move_to_long_algebraic(moves[i]) << " ; ";
-    }
-    int index;
-    std::cin >> index;
-    board->make_move(moves[index]);
+    utils::print_bitboard(board->get_piece_bitboard(i));
   }
 
-  // Searcher searcher{board};
-  // searcher.perft(4);
+  std::cout << board->get_white_king_loc() << " " << board->get_black_king_loc() << std::endl;
+
+
+  Searcher searcher{board};
+  int d;
+  std::cin >> d;
+  searcher.perft(d);
+  // board->reset(test_pos_2);
+  // searcher.perft(5);
+  // board->reset(test_pos_3);
+  // searcher.perft(5);
+  // board->reset(test_pos_4);
+  // searcher.perft(5);
+  // board->reset(test_pos_5);
+  // searcher.perft(5);
+  // board->reset(test_pos_6);
+  // searcher.perft(5);
+
+  // MoveGenerator gen{board};
+  // for ( ;; )
+  // {
+  //   std::cout << board->to_string();
+  //   std::cout << board->can_white_king_side_castle() << board->can_white_queen_side_castle() << board->can_black_king_side_castle() << board->can_black_queen_side_castle() << std::endl;
+  //   std::vector<Move> moves;
+  //   gen.generate_moves(moves);
+  //   for (int i = 0; i < moves.size(); i++)
+  //   {
+  //     std::cout << i << ": " << gen.move_to_long_algebraic(moves[i]) << " ; ";
+  //   }
+  //   int index;
+  //   std::cin >> index;
+  //   board->make_move(moves[index]);
+  // }
+
 
   
   // uci::start_uci_communication();

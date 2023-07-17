cd6d8323ad9069f206d0658c87d71d17a0d0258c
Jason Stentz <jstentz@andrew.cmu.edu>
2023-07-17 01:52:46 -0400

    even more inlining

--- include/bitboard.h
+++ include/bitboard.h
@@ -20,65 +20,93 @@
 
 typedef long long unsigned int bitboard;
 
-/**
- * @brief Rotate the bitboard 90 degrees clockwise.
- * 
- * @param b bitboard
- * @return Rotated bitboard 
- */
-bitboard rotate_90_clockwise(bitboard b);
+inline bitboard rotate_right(bitboard b, int s) {return (b >> s) | (b << (64 - s));}
 
-/**
- * @brief Rotate the bitboard 90 degrees anticlockwise.
- * 
- * @param b bitboard
- * @return Rotated bitboard 
- */
-bitboard rotate_90_anticlockwise(bitboard b);
+inline bitboard flip_vertical(bitboard b) {
+  return  ((b << 56)                               ) |
+      ((b << 40) & bitboard(0x00ff000000000000)) |
+      ((b << 24) & bitboard(0x0000ff0000000000)) |
+      ((b <<  8) & bitboard(0x000000ff00000000)) |
+      ((b >>  8) & bitboard(0x00000000ff000000)) |
+      ((b >> 24) & bitboard(0x0000000000ff0000)) |
+      ((b >> 40) & bitboard(0x000000000000ff00)) |
+      ((b >> 56));
+}
 
-/**
- * @brief Rotate the bitboard 45 degrees clockwise.
- * 
- * @param b bitboard
- * @return Rotated bitboard 
- */
-bitboard pseudo_rotate_45_clockwise(bitboard b);
+inline bitboard flip_diag_a1_h8(bitboard b) {
+  bitboard t;
+  const bitboard k1 = bitboard(0x5500550055005500);
+  const bitboard k2 = bitboard(0x3333000033330000);
+  const bitboard k4 = bitboard(0x0F0F0F0F00000000);
+  t = k4 & (b ^ (b << 28));
+  b ^= t ^ (t >> 28);
+  t = k2 & (b ^ (b << 14));
+  b ^= t ^ (t >> 14);
+  t = k1 & (b ^ (b << 7));
+  b ^= t ^ (t >> 7);
+  return b;
+}
 
-/**
- * @brief Rotate the bitboard 45 degrees anticlockwise.
- * 
- * @param b bitboard
- * @return Rotated bitboard 
- */
-bitboard pseudo_rotate_45_anticlockwise(bitboard b);
+inline bitboard rotate_90_clockwise(bitboard b) {
+  return flip_vertical(flip_diag_a1_h8(b));
+}
 
-/**
- * @brief Undo the 45 degree clockwise rotation.
- * 
- * @param b bitboard
- * @return Unrotated bitboard 
- */
-bitboard undo_pseudo_rotate_45_clockwise(bitboard b);
+inline bitboard rotate_90_anticlockwise(bitboard b) {
+  return flip_diag_a1_h8(flip_vertical(b));
+}
 
-/**
- * @brief Undo the 45 degree anticlockwise rotation.
- * 
- * @param b bitboard
- * @return Unrotated bitboard
- */
-bitboard undo_pseudo_rotate_45_anticlockwise(bitboard b);
+inline bitboard pseudo_rotate_45_clockwise(bitboard b) {
+  const bitboard k1 = bitboard(0xAAAAAAAAAAAAAAAA);
+  const bitboard k2 = bitboard(0xCCCCCCCCCCCCCCCC);
+  const bitboard k4 = bitboard(0xF0F0F0F0F0F0F0F0);
+  
+  b ^= k1 & (b ^ rotate_right(b, 8));
+  b ^= k2 & (b ^ rotate_right(b, 16));
+  b ^= k4 & (b ^ rotate_right(b, 32));
+  return b;
+}
+
+inline bitboard pseudo_rotate_45_anticlockwise(bitboard b) {
+  const bitboard k1 = bitboard(0x5555555555555555);
+  const bitboard k2 = bitboard(0x3333333333333333);
+  const bitboard k4 = bitboard(0x0F0F0F0F0F0F0F0F);
+  
+  b ^= k1 & (b ^ rotate_right(b, 8));
+  b ^= k2 & (b ^ rotate_right(b, 16));
+  b ^= k4 & (b ^ rotate_right(b, 32));
+  return b;
+}
+
+inline bitboard undo_pseudo_rotate_45_clockwise(bitboard b) {
+  for (size_t i = 0; i < 7; i++) {
+    b = pseudo_rotate_45_clockwise(b);
+  }
+  return b;
+}
+
+inline bitboard undo_pseudo_rotate_45_anticlockwise(bitboard b) {
+  for (size_t i = 0; i < 7; i++) {
+    b = pseudo_rotate_45_anticlockwise(b);
+  }
+  return b;
+}
+
+inline bitboard rem_first_bit(bitboard bits) {
+  return bits & (bits - 1);
+}
+
+inline int pop_count(bitboard b) {
+  if(b == 0) return 0;
+  const bitboard k1 = (bitboard)0x5555555555555555;
+  const bitboard k2 = (bitboard)0x3333333333333333;
+  const bitboard k4 = (bitboard)0x0F0F0F0F0F0F0F0F;
+  b = b - ((b >> 1) & k1);
+  b = (b & k2) + ((b >> 2) & k2);
+  b = (b + (b >> 4)) & k4;
+  b = (b * ((bitboard)0x0101010101010101)) >> 56;
+  return (int) b;
+}
 
 inline uint16_t first_set_bit(bitboard bits) {
   return constants::index64[((bits ^ (bits-1)) * constants::debruijn64) >> 58];
 }
-
-inline bitboard rotate_right(bitboard b, int s) {return (b >> s) | (b << (64 - s));}
-
-
-/**
- * @brief Given a bitboard, return the number of 1s in the bit pattern.
- * 
- * @param b bitboard
- * @return int 
- */
-int pop_count(bitboard b);

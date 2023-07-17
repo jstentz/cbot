156ac4a445775495b5a35c8f3f735558fe9ceb66
Jason Stentz <jstentz@andrew.cmu.edu>
2023-07-17 01:47:13 -0400

    inlined bitboard function

--- include/constants.h
+++ include/constants.h
@@ -271,4 +271,18 @@ inline static uint16_t IRR_PLY_OFFSET = 22;
 inline static uint64_t LAST_MOVE_MASK = 0xFFFFFFFF;
 inline static uint16_t LAST_MOVE_OFFSET = 32;
 
+inline int index64[64] = {
+  0, 47,  1, 56, 48, 27,  2, 60,
+   57, 49, 41, 37, 28, 16,  3, 61,
+   54, 58, 35, 52, 50, 42, 21, 44,
+   38, 32, 29, 23, 17, 11,  4, 62,
+   46, 55, 26, 59, 40, 36, 15, 53,
+   34, 51, 20, 43, 31, 22, 10, 45,
+   25, 39, 14, 33, 19, 30,  9, 24,
+   13, 18,  8, 12,  7,  6,  5, 63
+};
+
+const unsigned long long debruijn64 = 0x03f79d71b4cb0a89;
+
+
 }

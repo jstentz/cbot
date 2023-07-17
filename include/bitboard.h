59409d23acc1eca7ce3422699b6e3074fea3f89c
Jason Stentz <jstentz@andrew.cmu.edu>
2023-07-17 01:47:13 -0400

    inlined bitboard function

--- include/bitboard.h
+++ include/bitboard.h
@@ -13,6 +13,8 @@
 
 #include <cstdint>
 
+#include "include/constants.h"
+
 #define REMOVE_FIRST(a) ((a) = ((a) & ((a)-1)))
 #define BIT_FROM_SQ(a) ((bitboard)0x1 << a) // replace all luts.pieces with this
 
@@ -66,14 +68,9 @@ bitboard undo_pseudo_rotate_45_clockwise(bitboard b);
  */
 bitboard undo_pseudo_rotate_45_anticlockwise(bitboard b);
 
-/**
- * @author Kim Walisch (2012)
- * 
- * @param bits bitboard to scan
- * @precondition bits != 0 
- * @return index (0..63) of least significant one bit
- */
-uint16_t first_set_bit(bitboard bits); 
+inline uint16_t first_set_bit(bitboard bits) {
+  return constants::index64[((bits ^ (bits-1)) * constants::debruijn64) >> 58];
+}
 
 /**
  * @brief Given a bitboard, return the number of 1s in the bit pattern.
